c86c3fa3aebaf70ed31de677318132ca2d71870b
Reethi Sharon <162870081+ReethiSharon@users.noreply.github.com>
2025-02-11 11:02:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,11 @@ import pandas as pd
 st.title('🤖Machine Learning App')
 
 st.info('This is app building machine learning model')
-df=pd.read_csv('https://raw.githubusercontent.com/ReethiSharon/rs-machinelearning/refs/heads/master/data.csv')
-df
+
+with st.expander('Data'):
+   st.write('**Raw data**')
+   df=pd.read_csv('https://raw.githubusercontent.com/ReethiSharon/rs-machinelearning/refs/heads/master/data.csv')
+   df
 
 
 
